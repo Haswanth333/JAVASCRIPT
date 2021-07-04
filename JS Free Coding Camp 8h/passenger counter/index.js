847f8d438d706e9a9b9aa9ae59b5b48020079102
Haswanth333 <haswanthroyal333@gmail.com>
2021-07-04 07:10:26 +0530

    counter

--- JS Free Coding Camp 8h/passenger counter/index.js
+++ JS Free Coding Camp 8h/passenger counter/index.js
@@ -1,6 +1,57 @@
 // document.getElementById("count").innerText = 5;
-
 // let count = 5;
-
 // count = count + 2;
 // console.log(count);
+let count = 0;
+
+function increment() {
+  count = count + 1;
+  document.getElementById("count-el").innerText = count;
+  //   console.log("button clicked ");
+}
+
+// count = document.getElementById("count-el").innerText;
+
+// function decrement() {
+//   count = count - 1;
+//   document.getElementById("count-el").innerText = count;
+// }
+
+// let count = 0;
+
+// let lapscompleted = 0;
+
+// function counter() {
+//   lapscompleted = lapscompleted + 1;
+//   console.log(lapscompleted);
+// }
+
+// counter();
+
+// counter();
+
+// counter();
+
+// let lap1 = 34;
+// let lap2 = 33;
+// let lap3 = 36;
+
+// function logLaptime() {
+//   let totalTime = lap1 + lap2 + lap3;
+//   console.log(totalTime);
+// }
+
+// logLaptime();
+
+// console.log(totalTime);
+// race car count down
+
+// function countDown() {
+//   console.log(5);
+//   console.log(4);
+//   console.log(3);
+//   console.log(2);
+//   console.log(1);
+// }
+
+// countDown();
